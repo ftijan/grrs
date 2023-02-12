eb680968d0a1a3d5368578ee2dbb310e9cd7805e
Filip Tijan <ftijan@gmail.com>
2023-02-12 17:39:18 +0000

    refac to lib method

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#![allow(unused)]
-
 use clap::Parser;
 use anyhow::{Context, Result};
 
@@ -12,27 +10,12 @@ struct Cli {
     path: std::path::PathBuf
 }
 
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
-    for line in content.lines() {
-        if line.contains(pattern) {
-            writeln!(writer, "{}", line);
-        }
-    }
-}
-
 fn main() -> Result<()> {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("Could not read file `{}`", &args.path.display()))?;
         
-    find_matches(&content, &args.pattern, &mut std::io::stdout());
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
-}
-
-#[test]
-fn find_a_match() {
-    let mut result = Vec::new();
-    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
-    assert_eq!(result, b"lorem ipsum\n");
 }

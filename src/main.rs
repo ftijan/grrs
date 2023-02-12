db13bbe33c4b176812554f5b8bbeeae28f7789f9
Filip Tijan <ftijan@gmail.com>
2023-02-12 17:34:12 +0000

    refac for testing

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,27 @@ struct Cli {
     path: std::path::PathBuf
 }
 
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line);
+        }
+    }
+}
+
 fn main() -> Result<()> {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("Could not read file `{}`", &args.path.display()))?;
-    println!("file content:\n{}", content);
+        
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
+
     Ok(())
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
